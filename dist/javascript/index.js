440be8d6ed45c2edb26fe090cb21755382d32da9
cry <15293561@qq.com>
2018-06-23 23:49:49 +0800

    ban ben two sousou

--- dist/javascript/index.js
+++ dist/javascript/index.js
@@ -1,5 +1,6 @@
 
 
+
 $(function(){
 $(".tsan").hover(function(){
 	$(".tsan img").attr("src","images/txs.jpg")
@@ -38,5 +39,99 @@ $(".tphone").hover(function(){
 },function(){
 	$(".tph").hide()
 })
+
+$(".tsan").hover(function(){
+	$("#tlian").show()
+	$("#tlian").hover(function(){
+		$(this).show()
+	},function(){
+		$(this).hide()
+	})
+},function(){
+	$("#tlian").hide()
+})
+
+
+$("#t_nav li").click(function(){
+	$(this).css("background","white").siblings().css("background","#f6f6f6")
+})
+
+$(".tshi1 a").click(function(){
+	$(this).addClass("aa").siblings().removeClass("aa").parent().siblings().find("a").removeClass("aa");
+	$(".tnav_1").text($(this).text()).css("background","#f6f6f6")
+	$(".tshi1").hide();
+	$(".tcheng1").show();
+	$arr=$(this).text();
+	console.log($arr)
+	$(".tcheng1 a").click(function(){
+	$(this).addClass("aa").siblings().removeClass("aa");
+	$(".tnav_2").text($(this).text()).css("background","#f6f6f6")
+	$(".tcheng1").hide();
+	$(".txian1").show();
+	
+	$(".txian1 a").click(function(){
+	$(this).addClass("aa").siblings().removeClass("aa");
+	$(".tnav_3").text($(this).text()).css("background","#f6f6f6")
+	$(".tsan").text($arr);
+})
+})
+})
+
+
+$(".tnav_1").click(function(){
+	$(".tshi1").show();
+	$(".tcheng1").hide();
+	$(".txian1").hide();
+})
+
+$(".tnav_2").click(function(){
+	$(".tshi1").hide();
+	$(".tcheng1").show();
+	$(".txian1").hide();
+})
+$(".tnav_3").click(function(){
+	$(".tshi1").hide();
+	$(".tcheng1").hide();
+	$(".txian1").show();
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 })
